3330efb2bd4ef91c9c06fb6a2f6d7d03bb2611b3
helenvasquezsanchez@gmail.com <helenvasquezsanchez@gmail.com>
2024-11-08 20:57:54 -0500

    estilizando header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import '../styles/Header.css'
 
 function Header() {
     return (
-        <div>
+        <div className='headerContent'>
             <h1>Beatlemaniacos in the world</h1>
             <p>Somos un grupo de fans de los beatles, donde puedes compartir información, experiencias, discografía, datos curiosos y más.</p>
             
